2cfcf4146d6a2d955b55e742eed708388c6ac8dd
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-10-20 02:11:53 +0900

    refactor: consolidate proto files to Protos/

---
+++ envoy/grpc-nlb-envoy-eds/EdsServer/GrpcEdsService/Services/EnvoyClusterDiscoveryService.cs
@@ -0,0 +1,31 @@
+﻿using Envoy.Api.V2;
+using Envoy.Service.Discovery.V2;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcEdsService.Services
+{
+    public class EnvoyClusterDiscoveryService : ClusterDiscoveryService.ClusterDiscoveryServiceBase
+    {
+        private readonly ILogger<EnvoyClusterDiscoveryService> _logger;
+
+        public EnvoyClusterDiscoveryService(ILogger<EnvoyClusterDiscoveryService> logger)
+        {
+            _logger = logger;
+        }
+
+        // todo: get endpoint (GET)
+        // todo: add endpoint (POST)
+        // todo: update endpoint (PUT)
+        // todo: delete endpoint (DELETE)
+
+        public override Task<DiscoveryResponse> FetchClusters(DiscoveryRequest request, ServerCallContext context)
+        {
+            return base.FetchClusters(request, context);
+        }
+    }
+}
