534a1b92ac1c1157af58e68688f8057d0df626b6
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-12-18 16:18:19 +0900

    chore: deploy

--- csharp/grpc-unity-tls/k8s_deploy.sh
+++ csharp/grpc-unity-tls/k8s_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ACM=abcdefg
 DOMAIN=example.com
-kubectl kustomize ./k8s | sed -e "s|<acm>|${ACM}|g" -e "s|<domain>|${DOMAIN}|g"
+kubectl kustomize ./k8s | sed -e "s|<acm>|${ACM}|g" -e "s|<domain>|${DOMAIN}|g" | kubectl apply -f -
